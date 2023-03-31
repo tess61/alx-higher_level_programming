1bba41c1da37ea7e7d2b85ab48e462bd9ee228a6
tess61 <111118596+tess61@users.noreply.github.com>
2023-03-31 22:42:18 +0300

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""
-takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter
-"""
+"""takes in a letter and sends a POST request to
+with the letter as a parameter."""
+import requests
+from sys import argv
+
+
 if __name__ == '__main__':
-    import requests
-    from sys import argv
-    if len(argv) == 2:
-        q = argv[1]
-    else:
-        q = ""
-    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    url = "http://0.0.0.0:5000/search_user"
+    data = {'q': argv[1] if len(argv) >= 2 else ''}
     try:
-        r_dict = r.json()
-        id = r_dict.get('id')
-        name = r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
-            print("No result")
+        res = requests.post(url, data=data)
+        if res.json() == {}:
+            print('No result')
         else:
-            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
-    except:
-        print("Not a valid JSON")
+            user = res.json()
+            print(f'[{user.get("id")}] {user.get("name")}')
+    except ValueError:
+        print('Not a valid JSON')
